b05fe25974409c0c0a618bf3b47c50bb0bd0e8c7
Buki <Bukithompson@gmail.com>
2017-04-16 10:18:18 +0100

    setting up register on the backend - tested on insomis and rails c-works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :styles_created, class_name: "Style", foreign_key: "user_id"
   has_and_belongs_to_many :liked_styles, class_name: "Style", join_table: "styles_users"
   has_many :comments
 
   validates :name, presence: true, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   scope :api do
-    resources :comments
-    resources :styles
-    resources :types
-    resources :users
+      resources :comments
+      resources :styles
+      resources :types
+      resources :users, except: [:create]
+
+      post 'register', to: 'authentications#register'
+      post 'login', to: 'authentications#login'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
