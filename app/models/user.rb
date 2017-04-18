38cdd1d29c8be1864f0ca2cb8f3365aa11bc35ab
Buki <Bukithompson@gmail.com>
2017-04-18 09:57:54 +0100

    fixed oauth bug - github working now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false
   has_many :styles_created, class_name: "Style", foreign_key: "user_id"
   has_and_belongs_to_many :liked_styles, class_name: "Style", join_table: "styles_users"
   has_many :comments
